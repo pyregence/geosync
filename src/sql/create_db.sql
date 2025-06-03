74174571985e5e1d2b730391d479da4c8f8b110d
Oliver-BE <obaldwinedwards@gmail.com>
2025-06-03 10:29:52 -0400

    Update create_db.sql

--- src/sql/create_db.sql
+++ src/sql/create_db.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS :database;
 DROP ROLE IF EXISTS :user;
 CREATE ROLE :user WITH LOGIN CREATEDB PASSWORD :'password';
+GRANT :user TO postgres;
+SET ROLE :user;
 CREATE DATABASE :database WITH OWNER :user;
 \c :database
 CREATE EXTENSION postgis;
